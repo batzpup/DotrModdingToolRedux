b6ab2838c960978d5c2a3f91641811f77bd271cf
Adam Vernali <57483922+batzpup@users.noreply.github.com>
2025-02-12 21:46:37 +1100

    Test update

--- GameplayPatches/PatchByteSequences.cs
+++
@@ -1,8 +0,0 @@
-namespace GameplayPatches {
-    public class PatchByteSequences
-    {
-        public int Offset { get; set; }
-        public byte[] Original { get; set; }
-        public byte[] Patch { get; set; }
-    }
-}
--- IImGuiWindow.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DotrModdingTool2IMGUI;
-
-public interface IImGuiWindow
-{
-    public void Render();
-    public void Free();
-
-    
-}
